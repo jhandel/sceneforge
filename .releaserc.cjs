6dcde0d989d94d53e2321612f431baa37785ffd2
Josh Handel <josh@liveoak.ws>
2026-01-20 15:54:42 -0600

    fix: bypass npm workspace versioning

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -4,12 +4,12 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    ["@semantic-release/exec", { prepareCmd: "bun run build:sceneforge" }],
     [
-      "@semantic-release/npm",
+      "@semantic-release/exec",
       {
-        pkgRoot: "packages/sceneforge",
-        npmPublish: true,
+        prepareCmd:
+          "bun run build:sceneforge && cd packages/sceneforge && npm version ${nextRelease.version} --no-git-tag-version --allow-same-version --workspaces=false",
+        publishCmd: "cd packages/sceneforge && npm publish --access public",
       },
     ],
     "@semantic-release/github",
